58a259c1085de6b81062596012f8c2ad88e57113
Jonas Schneider-Bensch <jonas@cryspen.com>
2026-02-24 15:56:57 +0100

    [aes] CCM: Add self-tests for different AAD lengths

--- crates/algorithms/aes/tests/self.rs
+++ crates/algorithms/aes/tests/self.rs
@@ -42,3 +42,69 @@ fn ptxt_too_long() {
     let e = k.decrypt(pt, &nonce, b"", &mut [], &tag).unwrap_err();
     assert_eq!(e, DecryptError::PlaintextTooLong);
 }
+
+#[test]
+fn ccm_two_byte_aad_len_encoding() {
+    use libcrux_aes::{AeadConsts as _, AesCcm128};
+
+    let k: Key = [0; AesCcm128::KEY_LEN].into();
+    let nonce: Nonce = [0; AesCcm128::NONCE_LEN].into();
+    let mut tag: Tag = [0; AesCcm128::TAG_LEN].into();
+
+    // unsafely create a slice that is too long
+    let aad = vec![8; 512];
+
+    let pt = [42u8; 1];
+    let mut ct = [0u8; 1];
+    k.encrypt(&mut ct, &mut tag, &nonce, &aad, &pt).unwrap();
+
+    let mut pt_decrypted = [0u8; 1];
+    k.decrypt(&mut pt_decrypted, &nonce, &aad, &ct, &tag)
+        .unwrap();
+
+    assert_eq!(pt, pt_decrypted);
+}
+
+#[test]
+fn ccm_six_byte_aad_len_encoding() {
+    use libcrux_aes::{AeadConsts as _, AesCcm128};
+
+    let k: Key = [0; AesCcm128::KEY_LEN].into();
+    let nonce: Nonce = [0; AesCcm128::NONCE_LEN].into();
+    let mut tag: Tag = [0; AesCcm128::TAG_LEN].into();
+
+    // 2^16 - 2^8 - 1 = 65279
+    let aad = vec![8; 65279];
+
+    let pt = [42u8; 1];
+    let mut ct = [0u8; 1];
+    k.encrypt(&mut ct, &mut tag, &nonce, &aad, &pt).unwrap();
+
+    let mut pt_decrypted = [0u8; 1];
+    k.decrypt(&mut pt_decrypted, &nonce, &aad, &ct, &tag)
+        .unwrap();
+
+    assert_eq!(pt, pt_decrypted);
+}
+
+#[test]
+#[cfg(target_pointer_width = "64")]
+fn ccm_ten_byte_aad_len_encoding() {
+    use libcrux_aes::{AeadConsts as _, AesCcm128};
+
+    let k: Key = [0; AesCcm128::KEY_LEN].into();
+    let nonce: Nonce = [0; AesCcm128::NONCE_LEN].into();
+    let mut tag: Tag = [0; AesCcm128::TAG_LEN].into();
+
+    let aad = vec![8; u32::MAX as usize + 1];
+
+    let pt = [42u8; 1];
+    let mut ct = [0u8; 1];
+    k.encrypt(&mut ct, &mut tag, &nonce, &aad, &pt).unwrap();
+
+    let mut pt_decrypted = [0u8; 1];
+    k.decrypt(&mut pt_decrypted, &nonce, &aad, &ct, &tag)
+        .unwrap();
+
+    assert_eq!(pt, pt_decrypted);
+}
